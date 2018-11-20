9d2900594246a31b11eeeca0e01315d0aade3adb
bartoszptak <bartptak@gmail.com>
2018-11-20 18:12:52 +0100

    Add material design theme

--- SteganographyApp/MainWindow.xaml.cs
+++ SteganographyApp/MainWindow.xaml.cs
@@ -26,17 +26,9 @@ namespace SteganographyApp
             InitializeComponent();
         }
 
-        private void Load_button_Click(object sender, RoutedEventArgs e)
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            OpenFileDialog open_file_dialog = new OpenFileDialog
-            {
-                Filter = "Image files |*.png;*.jpg;*.bmp"
-            };
-
-            if (open_file_dialog.ShowDialog() == true)
-            {
-                image_box.Source = new BitmapImage(new Uri(open_file_dialog.FileName));
-            }
+            DragMove();
         }
     }
 }
